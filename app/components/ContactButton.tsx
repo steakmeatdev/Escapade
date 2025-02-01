5c87257d39462b2015abc1d28f130f4dc1e5a87a
Amine <149009322+steakmeatdev@users.noreply.github.com>
2025-02-01 10:54:02 +0100

    Landlord page UI

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,8 @@
+const ContactButton = () => {
+  return (
+    <div className="mt-6 py-4 px-6 cursor-pointer bg-escapade text-white rounded-xl hover:bg-escapade-dark transition">
+      Contact
+    </div>
+  );
+};
+export default ContactButton;
